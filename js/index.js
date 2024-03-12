3a667eb9ea17fd57318f0328e3161d1fa1bcf4f5
Jetistrim <paulogabriellima@hotmail.com>
2024-03-12 10:58:27 -0300

    requisição concluida

---
+++ js/index.js
@@ -0,0 +1,39 @@
+function listRegions (){
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes`)
+    .then(response => response.json())
+    .then(response => {
+        response.map(regiao => {
+            regioes.innerHTML += `<option value="${regiao.id}">${regiao.nome}</option>`
+        })
+    })
+    .finally(() => {
+        listStates()
+    })
+}
+listRegions()
+
+function listStates(){
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regioes.value}}/estados`)
+    .then(response => response.json())
+    .then(response => {
+        estados.innerHTML = '';
+        response.map(estado => {
+            estados.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`;
+        })
+    }
+    )
+}
+
+function listCitys(){
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estados.value}/municipios`)
+    .then(response => response.json())
+    .then(response => {
+        cidades.innerHTML = '';
+        response.map(cidade => {
+            cidades.innerHTML += `<option>${cidade.nome}</option>`;
+        })
+    }
+    )
+}
+
+// perguntar sobre orientação a objetos
